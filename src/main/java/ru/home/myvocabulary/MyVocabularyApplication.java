637ddef75b9ab7ed7d62ac806e7d19f550548183
karomag <karomag88@mail.ru>
2024-01-07 14:40:40 +0300

    first commit

---
+++ src/main/java/ru/home/myvocabulary/MyVocabularyApplication.java
@@ -0,0 +1,13 @@
+package ru.home.myvocabulary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyVocabularyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyVocabularyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/home/myvocabulary/MyVocabularyApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.home.myvocabulary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyVocabularyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
